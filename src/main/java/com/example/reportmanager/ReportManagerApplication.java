1c526ed4e9dfa9e71ad1683c8a3fe73da520d1f3
kirill <kirill.syniavskyi@fluentcommerce.com>
2026-04-21 09:24:12 +0200

    basic project structure

---
+++ src/main/java/com/example/reportmanager/ReportManagerApplication.java
@@ -0,0 +1,12 @@
+package com.example.reportmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/reportmanager/ReportManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.reportmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
